8a596a06e97872c7c3c99290d9b7e5bad4a75517
ANGEL MANEL GÓEZ GIRALDO <angelgoez@Mac.lan>
2026-07-07 22:11:35 -0500

    Inital commit

---
+++ lib/mastery/core/question.ex
@@ -0,0 +1,49 @@
+defmodule Mastery.Core.Question do
+  alias Mastery.Core.Template
+
+  defstruct ~w[asked substitutions template]a
+
+  def new(%Template{} = template) do
+    template.generators
+    |> Enum.map(&build_substitution/1)
+    |> evaluate(template)
+  end
+
+  defp compile(template, substitutions) do
+    template.compiled
+    |> Code.eval_quoted(assigns: substitutions)
+    |> elem(0)
+  end
+
+  defp evaluate_v1(substitutions, template) do
+    {asked, _bound} = Code.eval_quoted(template.compiled, assigns: substitutions)
+
+    %__MODULE__{
+      asked: asked,
+      substitutions: substitutions,
+      template: template
+    }
+  end
+
+  defp evaluate(substitutions, template) do
+    %__MODULE__{
+      asked: compile(template, substitutions),
+      substitutions: substitutions,
+      template: template
+    }
+  end
+
+  defp build_substitution({name, choices_or_generator}) do
+    IO.inspect(name)
+    IO.inspect(choices_or_generator)
+    {name, choose(choices_or_generator)}
+  end
+
+  defp choose(choices) when is_list(choices) do
+    Enum.random(choices)
+  end
+
+  defp choose(generator) when is_function(generator) do
+    generator.()
+  end
+end
---
+++ lib/mastery/core/quiz.ex
@@ -0,0 +1,66 @@
+defmodule Mastery.Core.Quiz do
+  alias Mastery.Core.{Template, Question, Response}
+
+  defstruct title: nil,
+            mastery: 3,
+            templates: %{},
+            used: [],
+            current_question: nil,
+            last_response: nil,
+            record: %{},
+            mastered: []
+
+  def new(fields) do
+    struct!(__MODULE__, fields)
+  end
+
+  def add_template(quiz, fields) do
+    template = Template.new(fields)
+
+    templates =
+      update_in(
+        quiz.templates,
+        [template.category],
+        &add_to_list_or_nil(&1, template)
+      )
+
+    %{quiz | templates: templates}
+  end
+
+  defp add_to_list_or_nil(nil, template), do: [template]
+  defp add_to_list_or_nil(templates, template), do: [template | templates]
+
+  def select_question(%__MODULE__{templates: t}) when map_size(t) == 0, do: nil
+
+  def select_question(quiz) do
+    quiz
+    |> pick_current_question
+    |> move_template(:used)
+    |> reset_template_cycle
+  end
+
+  defp pick_current_question(quiz) do
+    Map.put(
+      quiz,
+      :current_question,
+      select_a_random_question(quiz)
+    )
+  end
+
+  defp select_a_random_question(quiz) do
+    quiz.templates
+    |> Enum.random()
+    |> elem(1)
+    |> Enum.random()
+    |> Question.new()
+  end
+
+  defp move_template(quiz, field) do
+  end
+
+  defp remove_template_from_category(quiz) do
+  end
+
+  defp add_template_to_field(quiz, field) do
+  end
+end
